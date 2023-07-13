efa4f9bb3d949d696bc43565c866ffd06b58592b
Gustavo Gomes <oliveiragusit@gmail.com>
2023-07-13 15:41:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+<script>
     document.getElementById('survey-form').addEventListener('submit', function(event) {
       var nameInput = document.getElementById('name');
       var emailInput = document.getElementById('email');
@@ -10,5 +10,7 @@
         alert('Por favor, preencha todos os campos obrigatórios!');
       }
     });
+</script>
+            
  
 
